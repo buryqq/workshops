9041eba3f2a7f9020818031164a11c7615859ddb
buryqq <dominik.burczynski@gmail.com>
2017-07-05 23:19:34 +0200

    avg_notes

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -3,6 +3,10 @@ class StudentDecorator < BaseDecorator
     "#{first_name} #{last_name}"
   end
 
-  def avg_notes(subject_item)
-   end
+ def avg_notes(subject_item)
+    notes = subject_item_notes.select { |note| note.subject_item_id == subject_item.id  }
+    notes.present? ? '%.2f' % (notes.sum(&:value).to_f / notes.length) : '0.00'	
+ end
+
+   
 end
