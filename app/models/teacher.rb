a7e22144406076355b90fa1455a7521f1b603b4f
buryqq <dominik.burczynski@gmail.com>
2017-07-05 16:43:51 +0200

    teacher model dependancies

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,9 +1,7 @@
 class Teacher < ActiveRecord::Base
   TITLES = %w(Dr. Prof. TA)
   has_many :subject_items, dependent: :nullify
-  has_many :subject_item_notes, dependent: :destroy
-  has_many :subject_items, dependent: :destroy
-
+  
   validates :first_name, :last_name, presence: true
   validates :academic_title, inclusion: { in: TITLES }, presence: true
 
