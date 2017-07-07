9fbf59d2467e94bcba85d2788433351364d24d76
buryqq <dominik.burczynski@gmail.com>
2017-07-07 15:18:23 +0200

    generate Payment resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170706190600) do
+ActiveRecord::Schema.define(version: 20170707131439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20170706190600) do
 
   add_index "participations", ["student_id"], name: "index_participations_on_student_id", using: :btree
   add_index "participations", ["subject_item_id"], name: "index_participations_on_subject_item_id", using: :btree
+
+  create_table "payments", force: :cascade do |t|
+    t.decimal  "amount"
+    t.date     "date"
+    t.integer  "student_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
---
+++ spec/factories/payments.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :payment do
+    amount ""
+date ""
+student_id 1
+  end
+
+end
